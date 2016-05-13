10d77788e4c2c4bddd489ca64a800b5dc7665d8f
zhangjiang <896884553@q.com>
2016-05-13 10:04:05 +0800

    Initial Commit

---
+++ ToolsTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToolsTestDemo
+//
+//  Created by pg on 16/5/13.
+//  Copyright © 2016年 DZHFCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToolsTestDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ToolsTestDemo
+//
+//  Created by pg on 16/5/13.
+//  Copyright © 2016年 DZHFCompany. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
